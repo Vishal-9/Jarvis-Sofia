8b3aac6578401e689c3aced086827fc0b4c401a8
Vishal-9 <gaikwadvishalgv@gmail.com>
2019-12-24 01:36:01 +0530

    jarvis-sofia

---
+++ jarvis.py
@@ -0,0 +1,72 @@
+import pyttsx3
+import datetime
+import speech_recognition as sr
+import wikipedia
+import webbrowser
+import os
+import random
+
+engine=pyttsx3.init('sapi5')
+voices=engine.getProperty('voices')
+
+engine.setProperty('voice',voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour=int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good morning!")
+    elif hour>=12 and hour<18:
+        speak("Good afternoon!")
+    else:
+        speak("Good evening!")
+
+    speak("Hello vishal!")
+def takeCommand():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold=0.5
+        audio=r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query=r.recognize_google(audio,language='en-in')   
+        print(f"User said:{query}\n")
+
+    except Exception as e:
+        print("Say that again please...")
+        return "None"         
+    return query
+if __name__ == "__main__":
+    wishMe()
+    while True:
+        query=takeCommand().lower()
+
+
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query=query.replace("wikipedia","")
+            results=wikipedia.summary(query,sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+        
+        elif 'play music' or 'play songs' in query:
+            music_dir='E:\\Songs'
+            songs=os.listdir(music_dir)
+            os.startfile(os.path.join(music_dir,songs[0]))
+            
+             
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open gmail' in query:
+            webbrowser.open("gmail.com")
